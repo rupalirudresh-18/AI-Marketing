24bb346c701aa99e2da0fe90cbda9a1c7529bbbd
RUPALI R <rupalirudresh18@gmail.com>
2026-05-29 17:16:53 +0530

    chnaged next.congif

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "5000",
-      },
-    ],
   },
 };
 
